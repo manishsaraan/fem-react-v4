13cb207fcef0e84703714e1e9673bce2cf446539
manishsaraan <manish021js@gmail.com>
2019-03-12 15:23:09 +0530

    find pets api added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import React from "react";
 import { render } from "react-dom";
+import pf from "petfinder-client";
 import Pet from "./Pet";
 
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pets: []
+    };
+  }
+
+  componentDidMount() {
+    petfinder.pet
+      .find({ output: "full", location: "Seattle, WA" })
+      .then(data => {
+        console.log(data);
+        let pets;
+        if (data.petfinder.pets && data.petfinder.pets.pet) {
+          if (Array.isArray(data.petfinder.pets.pet)) {
+            pets = data.petfinder.pets.pet;
+          } else {
+            pets = [data.petfinder.pets.pet];
+          }
+        }
+        this.setState({ pets });
+      });
+  }
   render() {
     return (
       <div>
