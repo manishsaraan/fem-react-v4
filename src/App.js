c43d57ee42d7f310b3b3238797d90334845cebe6
manishsaraan <manish021js@gmail.com>
2019-03-16 16:02:13 +0530

    react router added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { render } from "react-dom";
 import Results from "./Results";
+import Details from "./Details";
+import { Router } from "@reach/router";
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <h1>Adopt Me!</h1>
-        <Results />
+        <Router>
+          <Results path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     );
   }
---
+++ src/Details.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Details extends React.Component {
+  render() {
+    return <div>d</div>;
+  }
+}
+
+export default Details;
