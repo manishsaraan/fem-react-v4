2f618e37251b65b60388cb217f9f8445f70ff262
manishsaraan <manish021js@gmail.com>
2019-03-18 17:14:47 +0530

    showing animal details on details page

--- src/Details.js
+++ src/Details.js
@@ -1,8 +1,60 @@
 import React from "react";
+import pf from "petfinder-client";
+import { navigate } from "@reach/router";
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
 
 class Details extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true
+    };
+  }
+  componentDidMount() {
+    petfinder.pet
+      .get({ output: "full", id: this.props.id })
+      .then(data => {
+        const { pet } = data.petfinder;
+        console.log(pet);
+        let breed = pet.breeds.breed;
+        if (Array.isArray(pet.breeds.breed)) {
+          breed = pet.breeds.breed.join(", ");
+        }
+        this.setState({
+          name: pet.name,
+          animal: pet.animal,
+          location: `${pet.contact.city}, ${pet.contact.state}`,
+          description: pet.description,
+          media: pet.media,
+          breed,
+          loading: false
+        });
+      })
+      .catch(error => {
+        this.setState({ error, loading: false });
+        navigate("/");
+      });
+  }
   render() {
-    return <div>d</div>;
+    if (this.state.loading) {
+      return <h1>Loading...</h1>;
+    }
+
+    const { animal, breed, location, description, name } = this.state;
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {location}
+          </h2>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
   }
 }
 
