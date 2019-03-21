39ce6ca8c457a24028a662835a0448d966a70e54
manishsaraan <manish021js@gmail.com>
2019-03-21 20:58:02 +0530

    changing carousel image on click

--- src/Carousel.js
+++ src/Carousel.js
@@ -6,26 +6,34 @@ class Carousel extends React.Component {
     active: 0
   };
 
+  handleIndexClick = event => {
+    this.setState({ active: +event.target.dataset.index });
+  };
+
   static getDerivedStateFromProps({ media }) {
     let photos = [];
     if (media && media.photos && media.photos.photo) {
       photos = media.photos.photo.filter(photo => photo["@size"] === "pn");
     }
-    console.log(photos);
+
     return { photos };
   }
   render() {
     const { photos, active } = this.state;
+    console.log(photos);
     return (
       <div className="carousel">
         <img src={photos[active].value} alt="primary animal" />
         <div className="carousel-smaller">
           {photos.map((photo, index) => (
+            /* eslint-disable-next-line */
             <img
               alt="animal thumbnail"
+              data-index={index}
               key={photo.value}
               src={photo.value}
               className={index === active ? "active" : ""}
+              onClick={this.handleIndexClick}
             />
           ))}
         </div>
